8c1ce7566518b3450a163e7af04de477d4a819cb
pgutierrezd <pedrolgd99@gmail.com>
2022-09-23 19:36:39 -0400

    Vista asignatura agregada

---
+++ Pedro/controller.js
@@ -0,0 +1,15 @@
+subject = document.getElementById('subject')
+submit = document.getElementById('submit')
+
+submit.onclick = function (){
+    if(contains_number(subject.value)){
+        alert('El nombre de la asignatura no debe de contener números')
+    }    
+}
+
+function contains_number(txt){
+     for (i=0; i<txt.length; i++)
+        if(!isNaN(txt[i]))
+            return true
+    return false
+}

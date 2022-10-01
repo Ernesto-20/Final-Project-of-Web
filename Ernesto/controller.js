0306faa90fcbc448fb42dd68ddfe44d898b1b6a9
Ernesto-20 <ernestofarina702@gmail.com>
2022-10-01 17:15:56 -0300

    Upgrades in my form and some changes' git status git push adasd

--- Ernesto/controller.js
+++ Ernesto/controller.js
@@ -1,39 +1,40 @@
+const names = document.getElementById('name')
+const last_name = document.getElementById('last_name')
+const address = document.getElementById('address')
+const identification = document.getElementById('identification')
+const submit = document.getElementById('submit')
 
-names = document.getElementById('name')
-last_name = document.getElementById('last_name')
-address = document.getElementById('address')
-identification = document.getElementById('identification')
-submit = document.getElementById('submit')
-
-male_option = document.getElementById('male')
-female_option = document.getElementById('female')
-undefined_option = document.getElementById('undefined')
+const male_option = document.getElementById('male')
+const female_option = document.getElementById('female')
+const undefined_option = document.getElementById('undefined')
 
 female_option.checked = true
 
-male_option.onclick = function (){
-    if (female_option.checked)
+// Single Selection Validation
+document.querySelectorAll(".gender").forEach(el => {
+    el.addEventListener("click", e => {
+      const id = e.target.getAttribute("id");
+      let note =`El elemento seleccionado es ${id}`
+      console.log(note)
+      
+      if (id === 'male'){
         female_option.checked = false
-    else if(undefined_option.checked)
         undefined_option.checked = false
-}
-female_option.onclick = function (){
-    if (undefined_option.checked)
-        undefined_option.checked = false
-    else if(male_option.checked)
+      }
+      else if (id === 'female'){
         male_option.checked = false
-}
-undefined_option.onclick = function (){
-    if (female_option.checked)
+        undefined_option.checked = false
+      }
+      else if (id === 'undefined'){
         female_option.checked = false
-    else if(male_option.checked)
         male_option.checked = false
-}
+      }
+    });
+  });
 
 
-
+//   Simple Validation with alerts.
 submit.onclick = function (){
-
     if(contains_number(names.value)){
         alert('El nombre del estudiante no debe de contener números')
     }else if(contains_number(last_name.value)){
@@ -41,23 +42,26 @@ submit.onclick = function (){
     }else if(contains_lyrics(identification.value)){
         alert('El carnet del estudiante no debe de contener letas alfabéticas')
     }
-    
 }
 
+
+
 function contains_number(txt){
-    for (i=0; i<txt.length; i++){
+    let check = false
+    for (let i=0; i<txt.length && !check; i++){
         if(!isNaN(txt[i]))
-            return true
+            check = true
     }
 
-    return false
+    return check
 }
 
 function contains_lyrics(txt){
-    for (i=0; i<txt.length; i++){
+    let check = false
+    for (let i=0; i<txt.length && !check; i++){
         if(isNaN(txt[i]))
-            return true
+            check = true
     }
 
-    return false
+    return check
 }

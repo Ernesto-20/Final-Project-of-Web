741c5a1a11fefbf0192e306781429278bac7e8e5
Endlessand <sandytechboy00@gmail.com>
2022-12-04 16:46:38 -0500

    Add english i18n

--- project-frontend/src/main/java/cu/edu/cujae/pweb/dto/StudentGradeDTO.java
+++ project-frontend/src/main/java/cu/edu/cujae/pweb/dto/StudentGradeDTO.java
@@ -1,70 +1,73 @@
-//package cu.edu.cujae.pweb.dto;
+// package cu.edu.cujae.pweb.dto;
 //
-//import java.util.Map;
+// import java.util.Map;
 //
-//public class StudentGradeDTO {
+// public class StudentGradeDTO {
 //
-//    private Integer id;
-//    private String studentName;
+// private Integer id;
+// private String studentName;
 //
-//    // "subjectsGrades" se utiliza para mostrar las notas de las asignaturas si se
-//    // conoce el año
-//    // <Subject, Grade>
-//    private Map<String, Integer> subjectsGrades;
+// // "subjectsGrades" se utiliza para mostrar las notas de las asignaturas si
+// se
+// // conoce el año
+// // <Subject, Grade>
+// private Map<String, Integer> subjectsGrades;
 //
-//    // "yearsSubjectsGrades" se utiliza para almacenar todas las notas de las
-//    // asignaturas de todos los años
-//    // <Year, <Subject, Grade Value>>
-//    private Map<Integer, Map<String, Integer>> yearsSubjectsGrades;
+// // "yearsSubjectsGrades" se utiliza para almacenar todas las notas de las
+// // asignaturas de todos los años
+// // <Year, <Subject, Grade Value>>
+// private Map<Integer, Map<String, Integer>> yearsSubjectsGrades;
 //
-//    public StudentGradeDTO() {
-//        super(); // TODO Auto-generated constructor stub
-//    }
+// public StudentGradeDTO() {
+// super(); // TODO Auto-generated constructor stub
+// }
 //
-//    public StudentGradeDTO(Integer id, String studentName) {
-//        super();
-//        this.id = id;
-//        this.studentName = studentName;
-//    }
+// public StudentGradeDTO(Integer id, String studentName) {
+// super();
+// this.id = id;
+// this.studentName = studentName;
+// }
 //
-//    public StudentGradeDTO(Integer id, String studentName, Map<String, Integer> subjectsGrades,
-//            Map<Integer, Map<String, Integer>> yearsSubjectsGrades) {
-//        super();
-//        this.id = id;
-//        this.studentName = studentName;
-//        this.subjectsGrades = subjectsGrades;
-//        this.yearsSubjectsGrades = yearsSubjectsGrades;
-//    }
+// public StudentGradeDTO(Integer id, String studentName, Map<String, Integer>
+// subjectsGrades,
+// Map<Integer, Map<String, Integer>> yearsSubjectsGrades) {
+// super();
+// this.id = id;
+// this.studentName = studentName;
+// this.subjectsGrades = subjectsGrades;
+// this.yearsSubjectsGrades = yearsSubjectsGrades;
+// }
 //
-//    public Integer getId() {
-//        return id;
-//    }
+// public Integer getId() {
+// return id;
+// }
 //
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
+// public void setId(Integer id) {
+// this.id = id;
+// }
 //
-//    public String getStudentName() {
-//        return studentName;
-//    }
+// public String getStudentName() {
+// return studentName;
+// }
 //
-//    public void setStudentName(String studentName) {
-//        this.studentName = studentName;
-//    }
+// public void setStudentName(String studentName) {
+// this.studentName = studentName;
+// }
 //
-//    public Map<String, Integer> getSubjectsGrades() {
-//        return subjectsGrades;
-//    }
+// public Map<String, Integer> getSubjectsGrades() {
+// return subjectsGrades;
+// }
 //
-//    public void addSubjectsGrades(String subject, Integer gradeValue) {
-//        subjectsGrades.put(subject, gradeValue);
-//    }
+// public void addSubjectsGrades(String subject, Integer gradeValue) {
+// subjectsGrades.put(subject, gradeValue);
+// }
 //
-//    public Map<Integer, Map<String, Integer>> getYearsSubjectsGrades() {
-//        return yearsSubjectsGrades;
-//    }
+// public Map<Integer, Map<String, Integer>> getYearsSubjectsGrades() {
+// return yearsSubjectsGrades;
+// }
 //
-//    public void addYearsSubjectsGrades(Integer year, Map<String, Integer> subjectsGrades) {
-//        yearsSubjectsGrades.put(year, subjectsGrades);
-//    }
-//}
+// public void addYearsSubjectsGrades(Integer year, Map<String, Integer>
+// subjectsGrades) {
+// yearsSubjectsGrades.put(year, subjectsGrades);
+// }
+// }

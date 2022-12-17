1c7e4498626db311daca459898e49c8547c00ce9
Ernesto-20 <ernestofarina702@gmail.com>
2022-12-17 00:53:14 -0500

    Lock security

--- project-backend/src/main/java/cu/edu/cujae/backend/api/config/SecurityConfig.java
+++ project-backend/src/main/java/cu/edu/cujae/backend/api/config/SecurityConfig.java
@@ -71,9 +71,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/")
                 	.permitAll()
                 .antMatchers("/api/v1/auth/**")
-                	.permitAll();
-//                .anyRequest()
-//                	.authenticated();
+                	.permitAll()
+                .anyRequest()
+                	.authenticated();
 
         // Add our custom Token based authentication filter
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
